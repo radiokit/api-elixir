b4efce124eb6387d4080ae2a646cccfabc1e8423
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2016-04-12 14:07:40 +0200

    Bumped version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RadioKit.Mixfile do
 
   def project do
     [app: :radiokit_api,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
