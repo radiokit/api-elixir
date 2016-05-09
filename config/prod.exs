f5a0969459f607d3c053fa5135c9b02304da4f90
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2016-05-09 16:28:06 +0200

    Moved API endpoints to config files

---
+++ config/prod.exs
@@ -0,0 +1,5 @@
+use Mix.Config
+
+config :radiokit_api,
+  vault_base_url: "https://radiokit-vault-prod.herokuapp.com",
+  plumber_base_url: "https://radiokit-plumber-prod.herokuapp.com"
---
+++ config/stag.exs
@@ -0,0 +1,5 @@
+use Mix.Config
+
+config :radiokit_api,
+  vault_base_url: "https://radiokit-vault-stag.herokuapp.com",
+  plumber_base_url: "https://radiokit-plumber-stag.herokuapp.com"
