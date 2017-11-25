0306e453708fe7a734b675691efeb4316e41cd00
Marcin Lewandowski <marcin@radiokit.org>
2017-11-25 15:24:31 +0100

    Bumped to 0.3.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RadioKit.Mixfile do
 
   def project do
     [app: :radiokit_api,
-     version: "0.3.1",
+     version: "0.3.2",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :stag or Mix.env == :prod,
      start_permanent: Mix.env == :stag or Mix.env == :prod,
